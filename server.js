0ccfbcc66299108e1aa4a1940828da6df2471dfd
cmlaiii <103226707+f051a0303@users.noreply.github.com>
2023-03-14 23:19:34 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,5 @@
-/*********************************************************************************
-*  WEB422 – Assignment 1
-*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
-*  No part of this assignment has been copied manually or electronically from any other source
-*  (including web sites) or distributed to other students.
-* 
-*  Name: ___Chi Ming Lai_________ Student ID: __158400200____________ Date: ______15 September, 2022__________
-*  Cyclic Link: _______________________________________________________________
-*
-********************************************************************************/ 
+
+
 
 const express = require("express");
 const app = express();
